656fb2d8c3ec11b86e7803a94a3ce8aa4798ea97
ssuyu1220 <69281366+ssuyu1220@users.noreply.github.com>
2025-05-13 02:58:57 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-ldapsearch -x -H ldaps://ldap.35.nasa '(&(objectClass=posixAccount)(uid='"$1"'))' 'sshPublicKey' | sed -n '/^ /{H;d};/sshPublicKey:/x;$g;s/\n *//g;s/sshPublicKey: //gp'
+export LDAPTLS_REQCERT=never
+exec /usr/bin/ldapsearch -x -LLL -H ldaps://localhost \
+  -b "ou=People,dc=35,dc=nasa" \
+  "(&(objectClass=posixAccount)(uid=$1))" sshPublicKey 2>/dev/null | \
+  sed -n '/^ /{H;d};/sshPublicKey:/x;$g;s/\n *//g;s/sshPublicKey: //gp'
