40154e82ec72004500301190bd5e0f2ac9b34c77
ssuyu1220 <69281366+ssuyu1220@users.noreply.github.com>
2025-05-24 21:42:34 +0800

    Update singlethread.c

--- singlethread.c
+++ singlethread.c
@@ -6,18 +6,11 @@
 #define M 800
 #define K 800
 #define N 800
-#define NUM_THREADS 4
 
 // matrix_a(M*K)*matrix_a(K*N)=matrix_c(M*N)
 int matrix_a[M][K];
 int matrix_b[K][N];
 int matrix_c[M][N];
-
-typedef struct {
-    int start_row;
-    int end_row;
-    int id; // thread id
-} ThreadArg;
 
 // calaulate matrix_a*matrix_b=matrix_c
 void matrix_multiply() {
@@ -34,27 +27,30 @@ void matrix_multiply() {
 
 int main() {
     // implement data of matrix_a
-    for (int i = 0; i < M; i++)
-        for (int j = 0; j < K; j++)
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < K; j++) {
             matrix_a[i][j] = i + j;
-
+        }
+    }
     // implement data of matrix_b
-    for (int i = 0; i < K; i++)
-        for (int j = 0; j < N; j++)
+    for (int i = 0; i < K; i++) {
+        for (int j = 0; j < N; j++) {
             matrix_b[i][j] = abs(i - j);
+        }
+    }
 
     struct timespec start, end;
-    clock_gettime(CLOCK_MONOTONIC, &start);
+    clock_gettime(CLOCK_MONOTONIC, &start); // start timer
 
     matrix_multiply();
 
-    // write result to singlethread_matrix_c.txt
+    // open file
     FILE *file = fopen("singlethread_matrix_c.txt", "w");
     if (file == NULL) {
         perror("Failed to open file");
         exit(1);
     }
-
+    // write result to singlethread_matrix_c.txt
     for (int i = 0; i < M; ++i) {
         for (int j = 0; j < N; ++j) {
             fprintf(file, "%d ", matrix_c[i][j]);
@@ -64,10 +60,11 @@ int main() {
     fclose(file);
     printf("matrix_c has been written to singlethread_matrix_c.txt\n");
 
-    clock_gettime(CLOCK_MONOTONIC, &end);
+    clock_gettime(CLOCK_MONOTONIC, &end); // end timer
     double dur =
         (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
-    printf("Multiplication done. Total time: %lf seconds\n", dur);
+    printf("total time of matrix multiplication (single thread): %lf secs\n",
+           dur);
 
     return 0;
 }
